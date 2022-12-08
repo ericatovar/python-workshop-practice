0240af09692fa33b754fa1d9747e0a143feeeac4
ericatovar <ericatovar99@gmail.com>
2022-12-08 14:07:04 -0600

    Revert "Revert "Revert "First commit"""
    This reverts commit 3ce4e05c8f526db7e2a376574a0990d4555d2839.

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (2000, 'hello', '2000-01-01 00:00:00')
+
+--UPDATE sandbox SET int32_value = -14534 where id = 113;
+
+--DELETE FROM sandbox WHERE id = 118;
+
+SELECT * FROM sandbox;
